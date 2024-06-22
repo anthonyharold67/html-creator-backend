23c8b870222a9fd6aa237e05d91b27a0d845c1b5
anthonyharold67 <93030628+anthonyharold67@users.noreply.github.com>
2024-06-22 16:01:37 +0300

    "Updated admin.py to use DateRangeFilter and DateTimeRangeFilter, added search_fields, and modified formats. Added 'rangefilter' to INSTALLED_APPS in settings.py and set STATIC_ROOT. Updated requirements.txt to include new dependencies."

--- html_creator/admin.py
+++ html_creator/admin.py
@@ -4,14 +4,18 @@ from import_export.admin import ExportMixin
 from .models import HtmlCreatorModel
 from django.contrib.admin.filters import DateFieldListFilter
 from import_export.formats import base_formats
+from rangefilter.filters import DateRangeFilter, DateTimeRangeFilter
 
 class HtmlCreatorAdmin(ExportMixin, admin.ModelAdmin):
     list_display = ('entity_id', 'created_at', 'last_updated')
     list_filter = (
-        ('created_at', DateFieldListFilter),
-        ('last_updated', DateFieldListFilter),
+        ('created_at', DateRangeFilter),
+        ('created_at', DateTimeRangeFilter),
+        ('last_updated', DateRangeFilter),
+        ('last_updated', DateTimeRangeFilter),
         # Diğer filtreleriniz
     )
+    search_fields = ('entity_id',)
     # Eğer özelleştirilmiş bir dışa aktarma formatı istiyorsanız, bu ayarı yapın
     formats = [base_formats.XLSX, base_formats.CSV]
 
--- main/settings.py
+++ main/settings.py
@@ -38,10 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "html_creator",
-   'rest_framework',
+    'rest_framework',
     'django_filters',
     "corsheaders",
-     'import_export',
+    'import_export',
+    'rangefilter',
 ]
 
 MIDDLEWARE = [
@@ -122,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR / STATIC_URL
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
